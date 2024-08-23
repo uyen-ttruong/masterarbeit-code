2af6496c86c00edb68429a8fae0bd8551ac8048a
ttr <uyentotruong@gmail.com>
2024-08-23 06:32:36 +0200

    update date and map for bayern popoulation

--- bayernmap.py
+++ bayernmap.py
@@ -1,23 +1,57 @@
 import geopandas as gpd
+import pandas as pd
+import numpy as np
+import os
 import matplotlib.pyplot as plt
 
-# Load Bavaria GeoJSON file
-bavaria = gpd.read_file('bavaria.geojson')
+print(plt.style.available)
+plt.style.use('seaborn-v0_8')
 
-# Create the plot
-fig, ax = plt.subplots(figsize=(10, 10))
 
-# Plot Bavaria
-bavaria.plot(ax=ax, color='lightblue', edgecolor='black')
+zip_path = r"C:\Users\uyen truong\Desktop\Hochschule-Muenchen-LaTeX-Template\plz-5stellig.shp.zip"
 
-# Add title
-plt.title('Map of Bavaria', fontsize=16)
+shp_name = 'plz-5stellig'
 
-# Remove axis
-ax.axis('off')
+full_path = f"zip://{zip_path}!{shp_name}.shp"
 
-# Save the map as an image
-plt.savefig('bavaria_map.png', dpi=300, bbox_inches='tight')
+plz_shape_df = gpd.read_file(full_path, dtype={'plz': str})
+plt.rcParams['figure.figsize'] = [16, 11]
 
-# Display the plot (optional)
+
+plz_region_df = pd.read_csv(
+    'zuordnung_plz_ort.csv',
+    sep=',',
+    dtype={'plz': str}
+)
+
+plz_region_df.drop('osm_id', axis=1, inplace=True)
+#print(plz_region_df.head())
+# Merge data.
+germany_df = pd.merge(
+    left=plz_shape_df, 
+    right=plz_region_df, 
+    on='plz',
+    how='inner'
+)
+
+germany_df.drop(['note'], axis=1, inplace=True)
+
+bayern_df = germany_df.query('bundesland == "Bayern"')
+
+fig, ax = plt.subplots()
+
+bayern_df.plot(
+    ax=ax, 
+    column='einwohner', 
+    categorical=False, 
+    legend=True, 
+    cmap='autumn_r',
+)
+
+ax.set(
+    title='Bayern: Anzahl der Einwohner pro Postleitzahl', 
+    aspect=1.3, 
+    facecolor='lightblue'
+);
 plt.show()
+#germany_df.to_csv('germany_df.csv', index=False)
